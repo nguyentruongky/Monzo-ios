c9bd99b9f5f3b3d1cb779955264d53ee8cb9fd25
Ky Nguyen <nguyentruongky33@gmail.com>
2021-08-17 14:35:02 +0700

    Init project

---
+++ Monzo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monzo
+//
+//  Created by Ky Nguyen on 8/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
